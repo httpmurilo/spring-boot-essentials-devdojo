4139c81d69da92a902a68137d52261bac506861b
httpmurilo <muriloeduardo1997@outlook.com>
2021-11-19 20:11:26 -0300

    jpa tests

--- src/test/java/academy/murilo/springboot2/repository/AnimeRepositoryTest.java
+++ src/test/java/academy/murilo/springboot2/repository/AnimeRepositoryTest.java
@@ -1,6 +1,7 @@
 package academy.murilo.springboot2.repository;
 
 import academy.murilo.springboot2.domain.Anime;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,19 @@ class AnimeRepositoryTest {
 
     @Test
     void save_PersistAnime_WhenSuccessful() {
+        Anime animeToBeSaved = createAnime();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+        Assertions.assertThat(animeSaved).isNotNull();
+        Assertions.assertThat(animeSaved.getId()).isNotNull();
+    }
+    @Test
+    void save_UpdatesAnime_WhenSuccessful() {
+        Anime animeToBeSaved = createAnime();
+        Anime animeSaved = this.animeRepository.save(animeToBeSaved);
+        animeSaved.setName("overload");
+        Anime animeUpdate = this.animeRepository.save(animeSaved);
+        Assertions.assertThat(animeUpdate).isNotNull();
+        Assertions.assertThat(animeUpdate.getId()).isNotNull();
 
     }
 
