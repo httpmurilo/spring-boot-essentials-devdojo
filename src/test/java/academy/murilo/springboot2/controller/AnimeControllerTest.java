1e754fa0347386560ce976dbb6324d68b85993e6
httpmurilo <muriloeduardo1997@outlook.com>
2021-11-24 17:42:27 -0300

    encerramento

---
+++ src/test/java/academy/murilo/springboot2/controller/AnimeControllerTest.java
@@ -0,0 +1,15 @@
+package academy.murilo.springboot2.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class AnimeControllerTest {
+
+    @Test
+    void test(){
+
+    }
+}
